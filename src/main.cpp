19131a0b742112167e9d6b2dfef767a87ef15a9a
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-03 20:48:36 +0800

    modify parameter_3

--- src/main.cpp
+++ src/main.cpp
@@ -63,8 +63,8 @@ int main(){
     change_parameter["resource_ratio"] = {0.5, 1, 2, 10};
     change_parameter["area_alpha"] = {0.001, 0.01, 0.1}; 
     change_parameter["social_density"] = {0.25, 0.5, 0.75, 1}; 
-    change_parameter["new_request_cnt"] = {1, 5, 10, 15, 20};
-    change_parameter["num_of_node"] = {10, 100, 125, 150, 200};
+    change_parameter["new_request_cnt"] = {1};
+    change_parameter["num_of_node"] = {10};
 
     vector<string> X_names = {"num_of_node", "new_request_cnt"};//, "area_alpha", "resource_ratio", "entangle_alpha", "swap_prob"};
     vector<string> Y_names = {"waiting_time", "throughputs", "finished_throughputs", \
@@ -81,7 +81,7 @@ int main(){
     }
     
 
-    int round = 50;
+    int round = 5;
     for(string X_name : X_names) {
         map<string, double> input_parameter = default_setting;
 
