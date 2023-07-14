2045993f4c1f33ad468f07db6998c5e6654e9c37
Kash <andy151585@gmail.com>
2023-07-14 14:22:43 +0800

    rounding back

--- src/Algorithm/MyAlgo/MyAlgo.cpp
+++ src/Algorithm/MyAlgo/MyAlgo.cpp
@@ -497,6 +497,7 @@ void MyAlgo::rounding(){
                         }
                         index+=1;
                     }
+                    break;
                 }
             }
         }
