120ce0b711ad3d794ca56e7cadb1e6a62b918fa4
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-19 15:02:49 +0800

    revise parameter

--- src/Algorithm/MyAlgo/MyAlgo.cpp
+++ src/Algorithm/MyAlgo/MyAlgo.cpp
@@ -663,42 +663,63 @@ void MyAlgo::check_enough(vector<map<vector<int>, int>> &path){
 }  
 
 void MyAlgo::readd(vector<map<vector<int>, int>> &path,vector<int> &over_memory,map<vector<int>,int> &over_channel){
+    vector<pair<vector<int>, int>> re;
+    int max = -1;
+    for(unsigned int i = 0; i < requests.size(); i++){
+        for(auto it : path[i]){
+            if(max < it.second){
+                max = it.second;
+            }
+        }
+    }
+
+    for(int i = max; i >= 0; i--){
+        for(unsigned int j = 0; j < requests.size(); j++){
+            for(auto it : path[j]){
+                if(i == it.second){
+                    re.push_back({it.first, j});
+                }
+            }
+        }
+    }
     bool flag = true;
     while(flag){
         flag = false;
-        for(unsigned int i = 0; i < requests.size(); i++){
-            if(requests[i].get_send_limit() > requests[i].get_cur_send()){
-                for(auto it : path[i]){
-                    vector<int> each_path = it.first;
-                    bool assign = true;
-                    for(unsigned int j = 0; j < each_path.size() - 1; j++){
-                        if(j == 0){
-                            if(over_memory[each_path[j]] >= 0){
-                                assign = false;
-                            }
-                        }
-                        else{
-                            if(over_memory[each_path[j]] >= -1){
-                                assign = false;
-                            }
-                        }
-                        if(over_channel[{each_path[j],each_path[j+1]}] >= 0){
+        for(int i = 0; i < re.size(); i++){
+            if(requests[re[i].second].get_send_limit() > requests[re[i].second].get_cur_send()){
+                vector<int> each_path = re[i].first;
+                bool assign = true;
+                for(unsigned int j = 0; j < each_path.size() - 1; j++){
+                    if(j == 0){
+                        if(over_memory[each_path[j]] >= 0){
                             assign = false;
                         }
                     }
-                    if(over_memory[each_path[each_path.size()-1]] >= 0){
+                    else{
+                        if(over_memory[each_path[j]] >= -1){
+                            assign = false;
+                        }
+                    }
+                    if(over_channel[{each_path[j],each_path[j+1]}] >= 0){
                         assign = false;
                     }
-                    if(assign == true ){
-                        requests[i].add_cur(1);
-                        path[i][it.first] += 1;
-                        cout << "!!PATH +++" << endl;
-                        flag = true;
+                }
+                if(over_memory[each_path[each_path.size()-1]] >= 0){
+                    assign = false;
+                }
+                if(assign == true ){
+                    requests[re[i].second].add_cur(1);
+                    for(auto it:path[re[i].second]){
+                        if(it.first==re[i].first){
+                            path[i][it.first] += 1;
+                            cout << "!!PATH +++" << endl;
+                            flag = true;
+                            break;
+                        }
                     }
                 }
-            }else{
-                continue;
             }
+           
         }
     }
 }
