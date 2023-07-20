36cd39aa1e19255963ab0a4f3b6d2fe2e6468de0
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-20 21:06:27 +0800

    revise parameter

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ int main(){
     change_parameter["resource_ratio"] = {0.5, 1, 2, 10};
     change_parameter["area_alpha"] = {0.001, 0.01, 0.1}; 
     change_parameter["social_density"] = {0.25, 0.5, 0.75, 1}; 
-    change_parameter["new_request_cnt"] = {10};
+    change_parameter["new_request_cnt"] = {20};
     change_parameter["num_of_node"] = {5, 10, 15, 20, 25};
 
     vector<string> X_names = { "new_request_cnt"};//"num_of_node", "area_alpha", "resource_ratio", "entangle_alpha", "swap_prob"};
