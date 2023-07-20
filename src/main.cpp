874af814d0dd3e9f257fb091462eee6119c7ed5f
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-20 14:20:06 +0800

    revise parameter

--- src/main.cpp
+++ src/main.cpp
@@ -39,10 +39,10 @@ int main(){
     string file_path = "../data/";
 
     map<string, double> default_setting;
-    default_setting["num_of_node"] = 30;
+    default_setting["num_of_node"] = 100;
     default_setting["social_density"] = 0.5;
     default_setting["area_alpha"] = 0.1;
-    default_setting["memory_cnt_avg"] = 10;
+    default_setting["memory_cnt_avg"] = 12;
     default_setting["channel_cnt_avg"] = 5;
     default_setting["resource_ratio"] = 1;
     default_setting["min_fidelity"] = 0.7;
