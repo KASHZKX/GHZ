ac96d1ea21d46647fd1c8e243bc56ebc394aa7dc
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-20 18:53:50 +0800

    revise parameter

--- src/main.cpp
+++ src/main.cpp
@@ -48,8 +48,8 @@ int main(){
     default_setting["min_fidelity"] = 0.7;
     default_setting["max_fidelity"] = 0.95;
 
-    default_setting["swap_prob"] = 0.9;
-    default_setting["entangle_alpha"] = 0.0005;
+    default_setting["swap_prob"] = 0.8;
+    default_setting["entangle_alpha"] = 0.0002;
     default_setting["node_time_limit"] = 1;
     default_setting["new_request_cnt"] = 10;
     default_setting["request_time_limit"] = 1;
