17456e16750a6b455bc4ae922282eb8361d062cc
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-21 20:23:43 +0800

    revise parameter

--- src/Algorithm/REPS/REPS.cpp
+++ src/Algorithm/REPS/REPS.cpp
@@ -433,19 +433,16 @@ void REPS::entangle(){
         if(path.size() >= limit){
             for(int path_id = 0;path_id < limit; path_id++){
                 path[path_id]->entangle();
-                
             }
         }else{
             for(int path_id = 0; path_id < (int)path.size(); path_id++){
-                for(int path_id = 0;path_id < limit; path_id++){
                     path[path_id]->entangle();
             }
-            
         }
     }
 }
 
-}
+
 void REPS::send(){
     AlgorithmBase::base_send();
 }
@@ -454,7 +451,6 @@ void REPS::next_time_slot(){
 }
 
 void REPS::swap(){
-    cout <<"GG" << endl;
     //EPS based on Randomized Rounding
     EPS();
     //Entanglement Link Selection (ELS)
