17f227fbea20b3fa87ff00c3a30281bfae34de36
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-21 20:48:16 +0800

    revise parameter

--- src/Algorithm/QCAST/QCAST.cpp
+++ src/Algorithm/QCAST/QCAST.cpp
@@ -193,7 +193,7 @@ void QCAST::path_assignment(){
         assign_resource(candidate[mx_reqno].path, mx_reqno);
     }
 
-    find_recovery_path(3);
+    find_recovery_path(0);
     if(DEBUG) cerr<< "---------QCAST::path_assignment----------end" << endl;
 }
 
