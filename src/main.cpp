47cbf40cd4fa4c9262e54099581ce90a1f0fa7cf
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-23 03:38:18 +0800

    close brute revise parameter

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ int main(){
     string file_path = "../data/";
 
     map<string, double> default_setting;
-    default_setting["num_of_node"] = 30;
+    default_setting["num_of_node"] = 50;
     default_setting["social_density"] = 0.5;
     default_setting["area_alpha"] = 0.1;
     default_setting["memory_cnt_avg"] = 5;
@@ -67,7 +67,7 @@ int main(){
     change_parameter["resource_ratio"] = {0.5, 1, 2, 10};
     change_parameter["area_alpha"] = {0.001, 0.01, 0.1}; 
     change_parameter["social_density"] = {0.25, 0.5, 0.75, 1}; 
-    change_parameter["new_request_cnt"] = {5};
+    change_parameter["new_request_cnt"] = {5, 10, 15, 20};
 	change_parameter["request_avg"] = {5};
     change_parameter["num_of_node"] = {20, 25, 30, 40, 50};
 
