6fe4fb8a7f8098b0f3a1e968be940d0726738511
Ching-Ting-Wei <tim149557@gmail.com>
2023-07-28 03:44:29 +0800

    revise parameter

--- src/main.cpp
+++ src/main.cpp
@@ -95,10 +95,10 @@ int main(int argc, char *argv[]){
     change_parameter["request_avg"] = {3, 5, 7, 9, 11};
     change_parameter["num_of_node"] = {20, 30, 40, 50, 60};
     change_parameter["memory_cnt_avg"] = { 3, 5, 7, 9, 11};
-    vector<string> X_names =  {  "new_request_cnt"}; //"num_of_node","entangle_alpha", "resource_ratio","swap_prob" , "request_avg", "memory_cnt_avg" , "area_alpha" ,  "resource_ratio"
-    vector<string> Y_names =  {  "throughputs", "use_channel_ratio",  "use_memory_ratio","use_memory", "total_memory","use_channel", "total_channel" ,"throughput_memory_ratio", "throughput_channel_ratio"};
+    vector<string> X_names =  {  "new_request_cnt", "num_of_node","entangle_alpha", "resource_ratio","swap_prob" , "request_avg", "memory_cnt_avg" , "area_alpha"}; //
+    vector<string> Y_names =  { "max_over_ratio", "throughputs"};
                             // "use_channel_ratio",  "use_memory_ratio", "use_memory", "use_channel", "total_channel", "total_memory" "throughput_memory_ratio", "throughput_channel_ratio",
-                            // "S_D_complete_ratio_difference", "path_success_avg" , "max_over_ratio",
+                            // "S_D_complete_ratio_difference", "path_success_avg" , ,
                             // "path_success_avg_before_ent", "new_success_ratio"
 			                // "divide_cnt", "change_edge_num", "diff_edge_num", "diff_rate","edge_difference"
     vector<string> algo_names = {"Greedy_Nonlimit","QCAST_Nonlimit","REPS_Nonlimit", "MyAlgo3"};//{"MyAlgo3_0.100000", "MyAlgo3_0.200000", "MyAlgo3_0.400000","MyAlgo3_0.600000", "MyAlgo3_0.800000"}; //"MyAlgo", "MyGreedyAlgo", "MyAlgo2", 
